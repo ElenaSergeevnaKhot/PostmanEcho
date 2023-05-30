57ad6265b04d8088dce54244d1ebe30d5d892084
Elena <Elenakhot@mail.ru>
2023-05-30 12:49:11 +0300

    Initial commit

---
+++ src/test/java/org/example/PostmanEcho.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEcho {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    }
+
+
+
